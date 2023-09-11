0ff02cbc2bd1755f3e13181aea92b9d843e62c54
Naresh Kumar <nareshkumar3933@gmail.com>
2023-09-11 13:21:12 +0500

    commit

--- server/connection.js
+++ server/connection.js
@@ -1,28 +1,14 @@
+
+require("dotenv").config();
 const mongoose = require("mongoose");
 
-const connectDb = async () =>{
-
-    const connection = await mongoose.connect(MONGO_URI)
-
-    if(connection){
+const connectDb = async () => {
+    try {
+        const connection = await mongoose.connect(process.env.MONGO_URI);
         console.log("Database Connected");
-    }
-    else{
-        console.log("Database Not Connected");
+    } catch (error) {
+        console.error("Database Not Connected:", error);
     }
 }
 
-module.exports = {connectDb}
-
-// const mongoose = require("mongoose");
-
-// const connectDb = async () => {
-//     try {
-//         const connection = await mongoose.connect(process.env.MONGO_URI);
-//         console.log("Database Connected");
-//     } catch (error) {
-//         console.error("Database Not Connected:", error);
-//     }
-// }
-
-// module.exports = { connectDb };
+module.exports = { connectDb };
